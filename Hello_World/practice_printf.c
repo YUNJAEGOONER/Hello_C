c4a8109806f3443af9572f4f9c7ca505674433f9
YUNJAEGOONER <50546745+YUNJAEGOONER@users.noreply.github.com>
2021-10-28 13:18:48 +0900

    Create practice_printf.c
    practice, printf & format specifier

---
+++ Hello_World/practice_printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+int practice_1() {
+	printf("Hello, practice_1\n");
+	printf("1234567890\n");
+	//\n = Skip the line (줄 바꾸기)
+	return 0;
+}
+
+int practice_2() {
+	//printf("Hello, %s","practice_2");
+	printf("Hello, %s\n","practice_2");
+	//%s 자리에 String "world"가 들어간다
+	return 0;
+}
