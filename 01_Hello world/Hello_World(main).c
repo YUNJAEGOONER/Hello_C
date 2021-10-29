dfd9491130399d5bc3365dcd7ba1ed819d63267b
YUNJAEGOONER <50546745+YUNJAEGOONER@users.noreply.github.com>
2021-10-29 13:42:02 +0900

    Hello world main

---
+++ 01_Hello world/Hello_World(main).c
@@ -0,0 +1,10 @@
+#include <stdio.h> //C Standard Library
+
+void main() {
+	printf("Hello World");
+	format(); //Call fuction
+	practice_1();
+	practice_2();
+}
+//function's signature : return type, name of function, parameter 
+//main fuction is necessary in C (Only one)
