03f05fd10c89a8c90de2b6cfcaf75dc14f92683a
YUNJAEGOONER <50546745+YUNJAEGOONER@users.noreply.github.com>
2021-10-29 13:38:41 +0900

    variable&format_specifier

---
+++ 02_Variable/variable_and_format_specifier.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int variable() {
+
+	int year;   //declare variable
+	int month;  // 정수형 변수 선언
+	int day;
+
+	year = 1999; //assign value 
+	month = 8;   //변수에 값을 할당
+	day = 9;
+
+	printf("My birth day is %d/%d/%d\n", year, month, day);
+
+	return 0;
+}
+// declare variable = int(type of variable)  number(name of variable)
+// ex) int num1;
