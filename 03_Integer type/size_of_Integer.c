05b9d8ea885aaf262e26277c113fe43919642169
YUNJAEGOONER <50546745+YUNJAEGOONER@users.noreply.github.com>
2021-11-02 12:18:55 +0900

    size of Integer type

--- 03_Integer type/size_of_Integer.c
+++ 03_Integer type/size_of_Integer.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 int size() {
-	
+	int num1 = 0;
+	int size = sizeof num1;
+
+	printf("num1ÀÇÅ©±â:%dbyte(%dbit)\n", size, size*8);
+
+	return 0;
+}
+
+int integer_type_size(){
+	printf("char:%d, short:%d, int:%d, long:%d, long long:%d\n",
+		sizeof(char), sizeof(short), sizeof(int), sizeof(long), sizeof(long long));
+
 	return 0;
 }
