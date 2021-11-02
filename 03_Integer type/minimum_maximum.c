00f1916af580ab537870e23f8fe1f80655df4b93
YUNJAEGOONER <50546745+YUNJAEGOONER@users.noreply.github.com>
2021-11-02 13:52:03 +0900

    minimun&maximum of the Integer type (with over&under flow)

--- 03_Integer type/minimum_maximum.c
+++ 03_Integer type/minimum_maximum.c
@@ -44,3 +44,19 @@ int un_min_max() {
 
 	return 0;
 }
+
+int min_max_over_under_flow() {
+	//overflow는 저장할 수 있는 최댓값보다 큰 수를 할당한 경우에 발생 (최솟값부터시작)
+	//underflow는 저장할 수 있는 최솟값보다 작은 수를 할당한 경우에 발생(최댓값부터 시작)
+
+	//overflow & underflow in char
+	char num1 = CHAR_MAX + 1; //char 최댓값보다 1 큰 수를 할당 (char의 최솟값이 할당)
+	char num2 = CHAR_MIN - 1; //char 최솟값보다 1 작은 수를 할당 (char의 최댓값이 할당)
+
+	//overflow & underflow in unsinged char
+	unsigned char num3 = UCHAR_MAX + 1;//unsigned char 최댓값보다 1 큰 수를 할당(unsigned char의 최솟값이 할당)
+	unsigned char num4 = 0 - 1;//unsigned char 최솟값보다 1 작은 수를 할당(unsigned char의 최댓값이 할당)
+
+	printf("char_min: %d, char_max: %d, unsigned char_min: %u, unsigned char_max: %u\n",num1,num2,num3,num4);
+	return 0;
+}
