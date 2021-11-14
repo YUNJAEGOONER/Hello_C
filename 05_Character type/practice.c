232d842ab24ec4869912ac7622ce92caa67fe2ff
YUNJAEGOONER <50546745+YUNJAEGOONER@users.noreply.github.com>
2021-11-14 17:21:01 +0900

    practice

---
+++ 05_Character type/practice.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int practice1() {
+	printf("Practice1\n");
+	char c1 = '\t'; //Horizontal tab
+	//char c1 = 9;
+
+	printf("Hello");
+	printf("%c", c1);
+	printf("world\n");
+
+	return 0;
+}
+
+int practice2() {
+	printf("Practice2\n");
+
+	char c1 = 2;
+	char c2 = 5;
+
+	printf("%c\n", c1+48); 
+	printf("%c\n", c2 + 48);
+
+	return 0;
+}
