db058c2b7ca5c76b2539a64e1b09cbeb009bffd9
YUNJAEGOONER <50546745+YUNJAEGOONER@users.noreply.github.com>
2021-11-16 17:07:53 +0900

    main function for constant

---
+++ 06_Constant/constant_main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main() {
+	
+	char c1 = 116;
+	char lineFeed = 10;
+
+	printf("Hello Constan%c%c", c1, lineFeed);
+
+	literal_c();
+	constant();
+	integer_suffix();
+	real_number_suffix();
+}
