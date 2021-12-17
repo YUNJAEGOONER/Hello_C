30bc4752a0bdd714ac842bc0d0aff26df1fad81a
YUNJAEGOONER <50546745+YUNJAEGOONER@users.noreply.github.com>
2021-12-17 18:37:13 +0900

    Update recursive_function.c
    비트에 따른 최댓값 구하는 함수 추가

--- 실습/recursive_function.c
+++ 실습/recursive_function.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 void main(){
 	printf("Hello_World\n");
 	int num;
@@ -8,7 +9,14 @@ void main(){
 	printf("fibbo = %d\n", result);
 
 	int facto = factorial(num);
-	printf("factorial = %d", facto);
+	printf("factorial = %d\n", facto);
+
+	int u8;
+	scanf_s("%d", &u8);
+
+	int max = find_max(u8);
+	printf("%d비트로 표현할 수 있는 최댓값은 %d입니다", u8, max);
+
 }
 
 int fibbo(int n) {
@@ -24,6 +32,27 @@ int fibbo(int n) {
 			return fibbo(n - 1) + fibbo(n - 2);
 		}
 	}
+}
+
+int find_max(int bit){
+
+	int i = 1;
+	int result = 0;
+
+	int max = 1;
+
+	for (int j = 0; j < bit; j++) {
+		max = max * 2;
+	}
+
+	while (result < max - 1)
+	{
+		result = fibbo(i);
+		//printf("%d, %d\n", i, result);
+		i = i + 1;
+	}
+
+	return fibbo(i - 2);
 }
 
 int factorial(int n) {
