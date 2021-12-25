91528f1417e8a46e30a3277103fb4d576f5db103
YUNJAEGOONER <50546745+YUNJAEGOONER@users.noreply.github.com>
2021-12-25 21:07:24 +0900

    divisor & perfect_number
    정수의 약수를 구하고, 그 정수가 완전수인지 판별하기

---
+++ 실습/divisor.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int num = 75;
+int result = 2;
+
+void print(int* array,int index) {
+
+	result = 0;
+
+	printf("%d의 약수는", num);
+	for (int i = 0; i < index; i++) {
+		printf(" %d", array[i]);
+		result = result + array[i];
+	}printf("입니다.\n");
+
+	if (result == 2 * num) {
+		printf("%d는 완전수 입니다.\n", num);
+	}
+	else {
+		printf("%d는 완전수가 아닙니다.\n", num);
+	}
+}
+
+void main() {
+
+	int i = 1;
+	int index = 0;
+	int* array = malloc(sizeof(int) * 10);
+
+	while (i < result)
+
+	{
+		if (num % i == 0) {
+			result = num / i;
+			if (result == i) {
+				array[index] = i;
+			}
+			else {
+				array[index] = result;
+				array[index + 1] = i;
+				index = index + 2;
+			}
+			//printf("( %d %d )\n", result,i);
+		}
+		i = i + 1;
+	}
+	print(array, index);
+	printf("Hello_World\n");
+	
+}
